33aecaef5c266908cb5881e49fa9ed8594be809c
Margot Konig <margot.konig@blackrock.com>
2019-06-23 17:20:08 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,25 @@
 class Item
   attr_accessor :name, :price
+  
+  @@
 
   def initialize(name,price)
     @name = name
     @price = price
+  end
+  
+    def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+ 
+      item_name = req.path.split("/items/").last #turn /items/fruit into fruit
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
+ 
+    resp.finish
   end
 end
