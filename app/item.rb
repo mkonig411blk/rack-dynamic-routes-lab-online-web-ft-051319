cfa6c8dc080c8efbf8f5eb94e9539e3d536665d4
Margot Konig <margot.konig@blackrock.com>
2019-06-23 17:22:38 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -16,10 +16,11 @@ class Item
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last #turn /items/fruit into fruit
       if @@items.find{|s| s.name == item_name}
-      resp.write @@items.find{|s| s.name == item_name}.price
-    else 
-      resp.write "Route not found"
-      resp.status = 404
+        resp.write @@items.find{|s| s.name == item_name}.price
+      else 
+        resp.write "Route not found"
+        resp.status = 404
+      end
     end
     resp.finish
   end
